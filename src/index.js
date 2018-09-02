994d38caad6f9c33cdd995735a9104ce8eecf37e
JaburMacbookPro <jaburmail@gmail.com>
2018-09-02 00:52:59 -0300

    Criacao do repositorio

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import '../ReactotronConfig';
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import Routes from './routes';
+
+const App = () => (
+  <Routes />
+);
+
+export default App;
