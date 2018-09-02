39d4a4d5324767983f541148a32a0cd287cfba09
JaburMacbookPro <jaburmail@gmail.com>
2018-09-02 17:57:46 -0300

    Estilizacao da pagina Welcome

---
+++ src/styles/general.js
@@ -0,0 +1,12 @@
+import colors from './colors';
+import metrics from './metrics';
+
+export default {
+
+  box: {
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    padding: metrics.basePadding,
+  }
+
+}
